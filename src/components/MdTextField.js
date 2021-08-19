3592eb77bda9fe26da05797fa48d5ac1d75be662
黄祖斌 <huang-zb18@mails.tsinghua.edu.cn>
2021-08-19 18:41:42 +0800

    style: make h2 in md field orange

--- src/components/MdTextField.js
+++ src/components/MdTextField.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
+import style from "../css/markdown-styles.module.css";
+
 const StyledDiv = styled.div`
   line-height: 25px;
 `;
@@ -7,8 +9,8 @@ const StyledDiv = styled.div`
 const MdTextField = ({ text }) => {
   return (
     <div className="mx-2 my-10 sm:mx-5 lg:mx-10 xl:mx-20 px-2 sm:px-5 xl:px-20 lg:px-10">
-      <div className="bg-brown-400 px-8 pb-10 pt-3 rounded text-white ">
-        <StyledDiv>
+      <div className="bg-brown-400 px-8 pb-10 pt-3 rounded">
+        <StyledDiv className={style.reactMarkDown}>
           <ReactMarkdown>{text}</ReactMarkdown>
         </StyledDiv>
       </div>
