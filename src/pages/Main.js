d70c1b6ea121137b3b9f2f263579f084798dfc0d
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-11-17 00:29:29 +0800

    build: exchange and liquidity props

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,13 @@
-import { VideoPanel, Title, Documentation, Governance, Exchange, Liquidity, Farm, Launch } from "../components";
+import {
+	VideoPanel,
+	Title,
+	Documentation,
+	Governance,
+	Exchange,
+	Liquidity,
+	Farm,
+	Launch,
+} from "../components";
 import { Gradient } from "../atoms";
 import axios from "axios";
 import { useState } from "react";
@@ -12,10 +21,10 @@ const Main = () => {
 			<Gradient></Gradient>
 			<div className="relative" style={{ zIndex: 1 }}>
 				<div className="mx-2 mb-20 sm:mx-5 lg:mx-10 xl:mx-10 px-2 sm:px-5 xl:px-32 lg:px-8">
-					<Title iframeLoaded={iframeLoaded}></Title>
-					<VideoPanel setIframeLoaded={setIframeLoaded}></VideoPanel>
-					<Exchange></Exchange>
-					<Liquidity></Liquidity>
+					{/* <Title iframeLoaded={iframeLoaded}></Title> */}
+					{/* <VideoPanel setIframeLoaded={setIframeLoaded}></VideoPanel> */}
+					<Exchange iframeLoaded={iframeLoaded}></Exchange>
+					<Liquidity setIframeLoaded={setIframeLoaded}></Liquidity>
 					<Farm></Farm>
 					<Launch></Launch>
 					<Documentation></Documentation>
