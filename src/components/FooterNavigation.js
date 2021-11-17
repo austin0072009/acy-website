be5264df89e1bdf91b721a0da242453b317c6dbe
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-11-18 00:49:10 +0800

    style: update layout

--- src/components/FooterNavigation.js
+++ src/components/FooterNavigation.js
@@ -2,14 +2,15 @@ import { ACYLink } from "../atoms";
 
 const FooterNavigation = () => {
 	return (
-		<div className="p-2 w-full lg:w-auto">
+		<div className="p-2 w-full lg:w-auto flex flex-col container">
 			<div className="flex flex-row justify-between">
 				<ACYLink url="/about-us">ABOUT</ACYLink>
 				<ACYLink url="/ecosystem">ECOSYSTEM</ACYLink>
 				<ACYLink url="/governance">GOVERNANCE</ACYLink>
-				<ACYLink url="/acy-token">ACY TOKEN</ACYLink>
+				<ACYLink url="/acy-token">ACYTOKEN</ACYLink>
 			</div>
-			<div className="flex flex-row justify-items-end">
+			<div className="flex flex-row justify-end space-x-16">
+				<div></div>
 				<ACYLink url="/terms-of-use">TERMS</ACYLink>
 				<ACYLink url="/privacy-policy">PRIVACY</ACYLink>
 				<ACYLink url="/cookie-policy">COOKIE</ACYLink>
