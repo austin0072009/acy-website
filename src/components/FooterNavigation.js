81827aba47037f582c167447db83c16cf2d5a0e3
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-11-17 15:31:21 +0800

    update: added 3 new links

--- src/components/FooterNavigation.js
+++ src/components/FooterNavigation.js
@@ -1,19 +1,24 @@
 import { ACYLink } from "../atoms";
 
 const FooterNavigation = () => {
-  return (
-    <div className="p-5 w-full lg:w-auto">
-      <div className="flex flex-row justify-between">
-        <ACYLink url="/about-us">ABOUT</ACYLink>
-        <ACYLink url="/terms-of-use">TERMS</ACYLink>
-        <ACYLink url="/privacy-policy">PRIVACY</ACYLink>
-        <ACYLink url="/cookie-policy">COOKIE</ACYLink>
-      </div>
-      <p className="text-right" style={{ color: "#c9d2d8" }}>
-        &copy; 2021 ACY Finance
-      </p>
-    </div>
-  );
+	return (
+		<div className="p-2 w-full lg:w-auto">
+			<div className="flex flex-row justify-between">
+				<ACYLink url="/about-us">ABOUT</ACYLink>
+				<ACYLink url="/ecosystem">ECOSYSTEM</ACYLink>
+				<ACYLink url="/governance">GOVERNANCE</ACYLink>
+				<ACYLink url="/acy-token">ACY TOKEN</ACYLink>
+			</div>
+			<div className="flex flex-row justify-items-end">
+				<ACYLink url="/terms-of-use">TERMS</ACYLink>
+				<ACYLink url="/privacy-policy">PRIVACY</ACYLink>
+				<ACYLink url="/cookie-policy">COOKIE</ACYLink>
+			</div>
+			<p className="text-right" style={{ color: "#c9d2d8" }}>
+				&copy; 2021 ACY Finance
+			</p>
+		</div>
+	);
 };
 
 export default FooterNavigation;
