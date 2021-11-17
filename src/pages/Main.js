9c244af48af626c294ad33c738142c722b38fd41
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-11-17 18:18:10 +0800

    style: add styling

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -21,7 +21,7 @@ const Main = () => {
 			<Gradient></Gradient>
 			<div className="relative" style={{ zIndex: 1 }}>
 				<div className="mx-2 mb-20 sm:mx-5 lg:mx-10 xl:mx-10 px-2 sm:px-5 xl:px-32 lg:px-8">
-					{/* <Title iframeLoaded={iframeLoaded}></Title> */}
+					<Title></Title>
 					{/* <VideoPanel setIframeLoaded={setIframeLoaded}></VideoPanel> */}
 					<Exchange iframeLoaded={iframeLoaded}></Exchange>
 					<Liquidity setIframeLoaded={setIframeLoaded}></Liquidity>
