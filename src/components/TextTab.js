9f2540a0b019a43b7c211bd2085aee5725d552d3
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-11-17 15:32:09 +0800

    style: update layout

--- src/components/TextTab.js
+++ src/components/TextTab.js
@@ -7,10 +7,10 @@ const TextTab = ({ color, title, content, subtitle }) => {
 	return (
 		<div>
 			<div
-				className="relative text-white rounded-2xl py-2 md:py-6 px-4 md:px-10 justify-between mb-3"
+				className="relative text-white rounded-2xl py-2 md:py-4 px-4 md:px-8 justify-between mb-3"
 				style={{ backgroundColor: color }}
 			>
-				<p className="text-right ">{subtitle}</p>
+				<p className="text-right">{subtitle}</p>
 				<h2 className="text-5xl text-center">{title}</h2>
 				<div className="text-left">
 					<p>{content}</p>
