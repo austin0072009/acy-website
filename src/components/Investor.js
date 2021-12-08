64a5863702c80d8e8f1f5ce54924f60ec4aa4d51
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-12-08 21:08:57 +0800

    replace farm texttab flex settings

--- src/components/Investor.js
+++ src/components/Investor.js
@@ -6,14 +6,14 @@ import "./DualSlidingPanel.css";
 const Investor = () => {
 	return (
 		<div className="flex items-center justify-between">
-			<div className="flex flex-col items-center justify-between px-5 py-5">
+			<div className="flex flex-col mb-0 md:mb-0 items-center justify-between rounded-lg flex-grow gap-y-5 py-5">
 				<div className="grapes-container">
 					<Grapes></Grapes>
 				</div>
 				{/* <div className="mt-3 exchange-content">
 			There are there kind of farming functions. First one is PREMIER, that is to stake your liquidity for BTC，ETH,USDX and the project tokens. The second one is STANDARD, that is to stake your liquidity for ACY. The third one is ACYDAO, that is to stake your ACY token for ACY token.
 				</div> */}
-				<div className="mt-3 exchange-content">
+				<div className="flex flex-grow text-tab" style={{ width: "75%" }}>
 					<TextTab
 						title="THE BEST IDO FARM"
 						content="There are there kinds of farming functions. The first one is PREMIER, that is to stake your liquidity for BTC, ETH, USDX and the project tokens. The second one is STANDARD, that is to stake your liquidity for ACY. The third one is ACYDAO, that is to stake your ACY token for ACY token."
