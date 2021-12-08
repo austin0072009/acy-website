375bdb065283d72f8eb7b76aad628bef73276033
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-12-08 17:23:25 +0800

    corrected grammar

--- src/components/Investor.js
+++ src/components/Investor.js
@@ -2,28 +2,29 @@ import React from "react";
 import Grapes from "./Grapes";
 import { TextTab, JumpButton } from ".";
 
-
 const Investor = () => {
 	return (
-
-	<div className="flex items-center justify-between">
-			<div className="flex flex-col items-center justify-between px-5 py-5">	
-			<Grapes></Grapes> 
-			{/* <div className="mt-3 exchange-content">
+		<div className="flex items-center justify-between">
+			<div className="flex flex-col items-center justify-between px-5 py-5">
+				<Grapes></Grapes>
+				{/* <div className="mt-3 exchange-content">
 			There are there kind of farming functions. First one is PREMIER, that is to stake your liquidity for BTC，ETH,USDX and the project tokens. The second one is STANDARD, that is to stake your liquidity for ACY. The third one is ACYDAO, that is to stake your ACY token for ACY token.
 				</div> */}
 				<div className="mt-3 exchange-content">
 					<TextTab
 						title="THE BEST IDO FARM"
-						content="There are there kind of farming functions. First one is PREMIER, that is to stake your liquidity for BTC，ETH,USDX and the project tokens. The second one is STANDARD, that is to stake your liquidity for ACY. The third one is ACYDAO, that is to stake your ACY token for ACY token."
+						content="There are there kinds of farming functions. The first one is PREMIER, that is to stake your liquidity for BTC, ETH, USDX and the project tokens. The second one is STANDARD, that is to stake your liquidity for ACY. The third one is ACYDAO, that is to stake your ACY token for ACY token."
 					/>
 				</div>
-				<div className = 'farmLearn'>
-				<JumpButton text = {"Farm"}links = {"http://test.acy.finance/#/farms"}></JumpButton>
+				<div className="farmLearn">
+					<JumpButton
+						text={"Farm"}
+						links={"http://test.acy.finance/#/farms"}
+					></JumpButton>
 				</div>
-	
-	</div>
-	</div>);
+			</div>
+		</div>
+	);
 };
 
 export default Investor;
