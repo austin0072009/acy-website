c397d65e10ffee304e25b2894289fd587b75419b
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-12-10 22:04:42 +0800

    arrow shows up when panel is active

--- src/components/DualSlidingPanel.js
+++ src/components/DualSlidingPanel.js
@@ -21,14 +21,13 @@ const DualSlidingPanel = ({
 						{/* <JumpButton></JumpButton> */}
 					</div>
 					<div
-						class="panels__side panels__side--inner flex-col"
+						class="panels__side panels__side--inner flex flex-col items-center"
 						id="clickleft"
 						onClick={() => setPanelOneLeftActive(!panelOneLeftActive)}
 					>
 						<h1 class="panels__headline">CRYPTO TRADERS</h1>
 						<p>Enable traders to get the optimal output</p>
-						{/* <svg
-							
+						<svg
 							class="arrow arrow--left arrow-1"
 							width="4vw"
 							height="4vw"
@@ -36,7 +35,7 @@ const DualSlidingPanel = ({
 						>
 							<path d="M0 0h24v24h-24z" fill="none" />
 							<path d="M20 11h-12.17l5.59-5.59-1.42-1.41-8 8 8 8 1.41-1.41-5.58-5.59h12.17v-2z" />
-						</svg> */}
+						</svg>
 					</div>
 				</article>
 				<article class="panels__side panels__side--right">
@@ -48,16 +47,17 @@ const DualSlidingPanel = ({
 						<h1 class="panels__headline">LIQUIDITY MINERS</h1>
 						<p>Enable miners to earn more transaction fees</p>
 
-						{/* <svg
-							
-							class="arrow arrow--right arrow-1"
-							width="4vw"
-							height="4vw"
-							viewBox="0 0 24 24"
-						>
-							<path d="M0 0h24v24h-24z" fill="none" />
-							<path d="M12 4l-1.41 1.41 5.58 5.59h-12.17v2h12.17l-5.58 5.59 1.41 1.41 8-8z" />
-						</svg> */}
+						<div style={{ width: "100%" }}>
+							<svg
+								class="arrow arrow--right arrow-1"
+								width="4vw"
+								height="4vw"
+								viewBox="0 0 24 24"
+							>
+								<path d="M0 0h24v24h-24z" fill="none" />
+								<path d="M12 4l-1.41 1.41 5.58 5.59h-12.17v2h12.17l-5.58 5.59 1.41 1.41 8-8z" />
+							</svg>
+						</div>
 					</div>
 					<div class="panels__side panels__side--inner-right">
 						{/* <p>"The biggest benefit of Apollo was the inspiration it gave to a growing generation to get into science and aerospace."</p> */}
--- src/components/DualSlidingPanel2.js
+++ src/components/DualSlidingPanel2.js
@@ -26,9 +26,7 @@ const DualSlidingPanel2 = ({
 					>
 						<h1 class="panels__headline">CRYPTO FARMERS</h1>
 						<p>Enable farmers to earn the stable return</p>
-						{/* <svg
-							
-
+						<svg
 							class="arrow arrow--left arrow-2"
 							width="4vw"
 							height="4vw"
@@ -36,7 +34,7 @@ const DualSlidingPanel2 = ({
 						>
 							<path d="M0 0h24v24h-24z" fill="none" />
 							<path d="M20 11h-12.17l5.59-5.59-1.42-1.41-8 8 8 8 1.41-1.41-5.58-5.59h12.17v-2z" />
-						</svg> */}
+						</svg>
 					</div>
 				</article>
 				<article class="panels__side panels2__side--right">
@@ -47,8 +45,7 @@ const DualSlidingPanel2 = ({
 					>
 						<h1 class="panels__headline">CRYPTO INVESTORS</h1>
 						<p>Enable investors to seek the amazing profit</p>
-						{/* <svg
-							
+						<svg
 							class="arrow arrow--right arrow-2"
 							width="4vw"
 							height="4vw"
@@ -56,7 +53,7 @@ const DualSlidingPanel2 = ({
 						>
 							<path d="M0 0h24v24h-24z" fill="none" />
 							<path d="M12 4l-1.41 1.41 5.58 5.59h-12.17v2h12.17l-5.58 5.59 1.41 1.41 8-8z" />
-						</svg> */}
+						</svg>
 					</div>
 					<div class="panels__side panels2__side--inner-right">
 						{/* <p>"The biggest benefit of Apollo was the inspiration it gave to a growing generation to get into science and aerospace."</p> */}
