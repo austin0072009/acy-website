88d50daeaab4f19443c0880aecab5e70002a77d5
austin0072009 <2001beijing@163.com>
2021-12-11 14:05:53 +0800

    Arrow Delete

--- src/components/DualSlidingPanel2.js
+++ src/components/DualSlidingPanel2.js
@@ -26,12 +26,12 @@ const DualSlidingPanel2 = ({
 					>
 						<h1 class="panels__headline">CRYPTO FARMERS</h1>
 						<p>Enable farmers to earn the stable return</p>
-						<div className="arrow-container">
+						{/* <div className="arrow-container">
 							<svg class="arrow arrow--left arrow-2" viewBox="0 0 24 24">
 								<path d="M0 0h24v24h-24z" fill="none" />
 								<path d="M20 11h-12.17l5.59-5.59-1.42-1.41-8 8 8 8 1.41-1.41-5.58-5.59h12.17v-2z" />
 							</svg>
-						</div>
+						</div> */}
 					</div>
 				</article>
 				<article class="panels__side panels2__side--right">
@@ -42,12 +42,12 @@ const DualSlidingPanel2 = ({
 					>
 						<h1 class="panels__headline">CRYPTO INVESTORS</h1>
 						<p>Enable investors to seek the amazing profit</p>
-						<div className="arrow-container">
+						{/* <div className="arrow-container">
 							<svg class="arrow arrow--right arrow-2" viewBox="0 0 24 24">
 								<path d="M0 0h24v24h-24z" fill="none" />
 								<path d="M12 4l-1.41 1.41 5.58 5.59h-12.17v2h12.17l-5.58 5.59 1.41 1.41 8-8z" />
 							</svg>
-						</div>
+						</div> */}
 					</div>
 					<div class="panels__side panels2__side--inner-right">
 						{/* <p>"The biggest benefit of Apollo was the inspiration it gave to a growing generation to get into science and aerospace."</p> */}
