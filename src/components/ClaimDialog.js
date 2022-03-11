3e907fb3d94168492d7316c516c29019ff4405c6
MarkJiYuan <zhengjiy16@163.com>
2022-03-11 10:17:54 +0000

    use new claim contract

--- src/components/ClaimDialog.js
+++ src/components/ClaimDialog.js
@@ -1,57 +1,116 @@
 import { Dialog, Transition } from '@headlessui/react'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useWeb3React } from "@web3-react/core";
 import { getAddress } from '@ethersproject/address';
 import { Contract } from '@ethersproject/contracts';
 import { AddressZero } from '@ethersproject/constants';
 import { InjectedConnector } from '@web3-react/injected-connector';
 import airdropABI from "../contracts/abis/airdrop_abi.json";
+import erc20ABI from "../contracts/abis/erc20_abi.json";
 import "./ClaimDialog.css";
 
 const injected = new InjectedConnector({
-	supportedChainIds: [80001, 137, 56, 97],
+	supportedChainIds: [56],
 })
 
-const contractAddress = "0xf3BAa8F2698e8871D1f064A2A487248B248DE120";
+const contractAddress = "0x4304920820C642356F4dFd792B8108DE64Ea9CB9";
+const tokenAddress = "0xc94595b56e301f3ffedb8ccc2d672882d623e53a";
+
+const networks = {
+	bsc: {
+		chainId: `0x${Number(56).toString(16)}`,
+		chainName: "Binance Smart Chain Mainnet",
+		nativeCurrency: {
+			name: "Binance Chain Native Token",
+			symbol: "BNB",
+			decimals: 18
+		},
+		rpcUrls: [
+			"https://bsc-dataseed1.binance.org",
+			"https://bsc-dataseed2.binance.org",
+			"https://bsc-dataseed3.binance.org",
+			"https://bsc-dataseed4.binance.org",
+			"https://bsc-dataseed1.defibit.io",
+			"https://bsc-dataseed2.defibit.io",
+			"https://bsc-dataseed3.defibit.io",
+			"https://bsc-dataseed4.defibit.io",
+			"https://bsc-dataseed1.ninicoin.io",
+			"https://bsc-dataseed2.ninicoin.io",
+			"https://bsc-dataseed3.ninicoin.io",
+			"https://bsc-dataseed4.ninicoin.io",
+			"wss://bsc-ws-node.nariox.org"
+		],
+		blockExplorerUrls: ["https://bscscan.com"]
+	}
+};
 
 export function isAddress(value) {
-  try {
-    return getAddress(value);
-  } catch {
-    return false;
-  }
+	try {
+		return getAddress(value);
+	} catch {
+		return false;
+	}
 }
 
 // account is not optional
 export function getSigner(library, account) {
-  return library.getSigner(account).connectUnchecked();
+	return library.getSigner(account).connectUnchecked();
 }
 
 // account is optional
 export function getProviderOrSigner(library, account) {
-  return account ? getSigner(library, account) : library;
+	return account ? getSigner(library, account) : library;
 }
 
 // account is optional
 export function getContract(address, ABI, library, account) {
-  if (!isAddress(address) || address === AddressZero) {
-    throw Error(`Invalid 'address' parameter '${address}'.`);
-  }
+	if (!isAddress(address) || address === AddressZero) {
+		throw Error(`Invalid 'address' parameter '${address}'.`);
+	}
 
-  return new Contract(address, ABI, getProviderOrSigner(library, account));
+	return new Contract(address, ABI, getProviderOrSigner(library, account));
 }
-// 1. 先把claim功能在测试网上调好
-// 2. 改变claim model样式
-// 3. 更换cliam button样式
-// 4. 测试正式网claim
 
 function ClaimDialog({ isOpen, setIsOpen }) {
 
-	const [walletStatus, setWalletStatus] = useState('Please connect wallet first');
-	const [claimStatus, setClaimStatus] = useState('Not Claimed');
-	const { active, account, library, connector, activate, deactivate } = useWeb3React()
+	const { active, account, library, connector, activate, deactivate } = useWeb3React();
+	const [claimAmount, setClaimAmount] = useState(0);
+
+	useEffect(() => {
+		if (!active) {
+			setClaimAmount(0);
+			return;
+		}
+
+		const tokenContract = getContract(tokenAddress, erc20ABI, library, account);
+		tokenContract.allowance(contractAddress, account)
+			.then(res => {
+				setClaimAmount(res);
+				window.amount = res;
+			})
+			.catch(err => console.error(err));
+	}, [account])
+
+	const changeNetwork = async () => {
+		try {
+			if (!window.ethereum) throw new Error("No crypto wallet found");
+			await window.ethereum.request({
+				method: "wallet_addEthereumChain",
+				params: [
+					{
+						...networks["bsc"]
+					}
+				]
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
 	async function connect() {
+
+		await changeNetwork();
+
 		try {
 			await activate(injected)
 			console.log(library)
@@ -70,16 +129,20 @@ function ClaimDialog({ isOpen, setIsOpen }) {
 
 	// 0x0f355a70d27eF1376e77C6Df9d9D8d661bCDf339
 	const onClickClaim = () => {
-		const airdropContract = getContract(contractAddress, airdropABI, library, account);
-		window.contract = airdropContract;
-    airdropContract.claim()
+		const tokenContract = getContract(tokenAddress, erc20ABI, library, account);
+
+		if (claimAmount === 0 || claimAmount.toNumber() === 0) {
+			alert("Account not having allowance! Please check whether you get the prize or not.")
+			return;
+		}
+
+		tokenContract.transferFrom(contractAddress, account, claimAmount)
 			.then(res => {
-				console.log('claim result', res)
+				console.log(res);
 			})
-      .catch(e => {
-				alert("Claim is restricted to the winners! Please check this wallet is in the winner list.")
-        console.log('claim Error', e);
-      });		
+			.catch(err => {
+				console.error(err);
+			})
 	}
 
 	return (
@@ -91,7 +154,7 @@ function ClaimDialog({ isOpen, setIsOpen }) {
 			}}>
 				<div className="flex items-center justify-center min-h-screen">
 
-					<Dialog.Overlay className="fixed inset-0 bg-black opacity-50"/>
+					<Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
 
 					{/* This element is to trick the browser into centering the modal contents. */}
 					{/* <span
@@ -104,18 +167,19 @@ function ClaimDialog({ isOpen, setIsOpen }) {
 						<h1 id="claim-title">Claim ACY</h1>
 						<div>
 							<p>Please connect to wallet first.</p>
-							<div id="connect-button" onClick={connect}>Connect to wallet</div>
-							{account &&
-								<div id="connect-button" onClick={disconnect}>Disconnect</div>
+							{account ?
+								<div id="connect-button">Connected</div>
+								:
+								<div id="connect-button" onClick={connect}>Connect to wallet</div>
 							}
-							<p>Wallet Address: {account ? account : "Not Connected"}</p>
+							{/* {account &&
+								<div id="connect-button" onClick={disconnect}>Disconnect</div>
+							} */}
+							<p>Wallet Address: {account ? account : "Not Connected (BSC Mainnet)"}</p>
 						</div>
 
 						{account &&
-							<>
-								<button id="claim-button" onClick={onClickClaim}>Claim</button>
-								<button id="claim-button" onClick={() => setIsOpen(false)}>Cancel</button>
-							</>
+							<button id="claim-button" onClick={onClickClaim}>Claim</button>
 						}
 					</div>
 				</div>
